d1b25a3a08b4f14d0455b3fe7bea499a27ab80a1
Alexandr Lysenko <a.lysenko00@gmail.com>
2022-05-31 18:47:37 +0300

    refactor: abjust selectors according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.className='incomplete-item';
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
